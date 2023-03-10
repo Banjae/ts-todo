32c0a6fabe5f4e4db1435afd53f46be69aec6fed
devking <brand0610@naver.com>
2023-03-10 11:46:34 +0900

    firrbase 연동

--- src/style/style.ts
+++ src/style/style.ts
@@ -1,30 +1,30 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
-  position: relative;
-  max-width: 1024px;
-  margin: 0 auto;
-  margin-top: 50px;
-  background: #fff;
+    position: relative;
+    max-width: 560px;
+    margin: 0 auto;
+    margin-top: 20px;
+    background: #fff;
 `;
 
 export const Inner = styled.div`
-  padding: 30px;
+    padding: 30px;
 `;
 
 export const AppTitle = styled.h1`
-  text-align: center;
-  padding: 30px 0;
-  font-size: 30px;
-  color: cornflowerblue;
+    text-align: center;
+    padding: 30px 0;
+    font-size: 30px;
+    color: cornflowerblue;
 `;
 
 export const TodoInputWrap = styled.div`
-  width: 95%;
-  margin: 0 auto;
+    width: 95%;
+    margin: 0 auto;
 `;
 
 export const TodoListWrap = styled.div`
-  width: 95%;
-  margin: 0 auto;
+    width: 95%;
+    margin: 0 auto;
 `;
